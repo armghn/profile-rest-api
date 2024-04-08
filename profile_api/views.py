c7050960f14fcf8f3a6d77ade03ac69d854c56b5
Armaghan <armaghan_samare@yahoo.com>
2024-04-09 00:12:16 +0330

    feat: adeed post, put, patch and delete functions to the APIview.

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import HelloSerializer
 
 
 class HelloAPIView(APIView):
     """Test API View."""
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
@@ -17,5 +21,26 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
 
 
